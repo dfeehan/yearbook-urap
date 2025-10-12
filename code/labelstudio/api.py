b5102b545dc1b0e858fc731f801a3e6e45d6a69c
Jeremy Li <jeremyli@Jeremys-MacBook-Air.local>
2025-10-12 15:11:38 -0700

    dataframe in api.py

--- code/labelstudio/api.py
+++ code/labelstudio/api.py
@@ -1,6 +1,10 @@
 import requests
+import pandas as pd
 
+#Jeremy's API token. Consider using your own, though I'm not sure it really matters
 API_TOKEN = "6300902175076cfa5a76e10257490f71d689d15e"
+
+#
 TASK_ID = 185777725
 
 response = requests.get(
@@ -10,4 +14,32 @@ response = requests.get(
   },
 )
 
-print(response.json())
+data = response.json()
+#df = pd.DataFrame(data)
+#print(df)
+
+rows = []
+for annotation in data:
+    annotation_id = annotation['id']
+    created_by = annotation['created_username']
+    created_at = annotation['created_at']
+    
+    for result in annotation['result']:
+        rows.append({
+            'annotation_id': annotation_id,
+            'created_by': created_by,
+            'created_at': created_at,
+            'result_type': result.get('type'),
+            'from_name': result.get('from_name'),
+            'x': result.get('value', {}).get('x'),
+            'y': result.get('value', {}).get('y'),
+            'width': result.get('value', {}).get('width'),
+            'height': result.get('value', {}).get('height'),
+            'choices': str(result.get('value', {}).get('choices')),
+            'labels': str(result.get('value', {}).get('rectanglelabels')),
+        })
+
+df = pd.DataFrame(rows)
+print(df)
+print(df.iloc[169])
+
